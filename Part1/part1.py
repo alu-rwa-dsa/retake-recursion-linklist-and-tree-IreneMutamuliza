2b12fdc4be0bef5e5bf90ed1966834730fa474e8
IreneMutamuliza <77663853+IreneMutamuliza@users.noreply.github.com>
2021-07-09 10:57:04 +0200

    Add files via upload

---
+++ Part1/part1.py
@@ -0,0 +1,24 @@
+def recursion(string):
+
+    def funct(numbers):
+        if len(numbers) == 0:
+            return 0, 0
+
+        Odd, Even = funct(numbers[1:])
+
+        if int(numbers[0]) % 2 == 0:
+            Even += 1
+        else:
+            Odd += 1
+
+        return Odd, Even
+
+    Odd, Even = funct(string)
+
+    if Even < Odd:
+        return f'There are more odd numbers ({Odd}) or the same as even numbers ({Even}).'
+        
+    else:
+        return f'There are more even numbers ({Even}) than odd ({Odd}).'
+
+print(recursion("0987650"))
